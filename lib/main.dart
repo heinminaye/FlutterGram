0a630de931588d2a64d5e27ade5d1f6ff7afcb6b
heinminaye <heinminaye8000@gmail.com>
2022-07-08 11:03:14 +0630

    first

--- lib/main.dart
+++ lib/main.dart
@@ -25,100 +25,104 @@ class MyApp extends StatelessWidget {
         ),
         body: Container(
           color: Color.fromARGB(255, 229, 229, 229),
-          child: Container(
-            height: 370,
-            margin: const EdgeInsets.all(10),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(20)),
+          child: Card(
+            child: Container(
+              margin: const EdgeInsets.all(10),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: Column(mainAxisSize: MainAxisSize.min, children: [
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.only(left: 15),
+                          ),
+                          Image(
+                            image: AssetImage('images/circle.png'),
+                            width: 50,
+                            height: 50,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "Jennifer_Cole",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600, fontSize: 14),
+                          )
+                        ],
+                      ),
+                      Row(children: const [
+                        Icon(
+                          Icons.more_horiz,
+                          color: Colors.grey,
+                          size: 30,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 10),
+                        ),
+                      ])
+                    ]),
+                const SizedBox(
+                  height: 20,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: const Image(
+                    image: AssetImage('images/image.jpg'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.only(left: 15),
+                          ),
+                          Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "1,242",
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Icon(
+                            Icons.mode_comment_outlined,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "24",
+                          )
+                        ],
+                      ),
+                      Row(children: const [
+                        Icon(Icons.turned_in_not, size: 25),
+                        Padding(
+                          padding: EdgeInsets.only(right: 10),
+                        ),
+                      ])
+                    ]),
+              ]),
             ),
-            child: Column(children: [
-              const SizedBox(
-                height: 15,
-              ),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                Row(
-                  children: const [
-                    Padding(
-                      padding: EdgeInsets.only(left: 15),
-                    ),
-                    Image(
-                      image: AssetImage('images/circle.png'),
-                      width: 50,
-                      height: 50,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "Jennifer_Cole",
-                      style:
-                          TextStyle(fontWeight: FontWeight.w600, fontSize: 14),
-                    )
-                  ],
-                ),
-                Row(children: const [
-                  Icon(
-                    Icons.more_horiz,
-                    color: Colors.grey,
-                    size: 30,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(right: 10),
-                  ),
-                ])
-              ]),
-              const SizedBox(
-                height: 20,
-              ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: const Image(
-                  image: AssetImage('images/image.jpg'),
-                  width: 340,
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                Row(
-                  children: const [
-                    Padding(
-                      padding: EdgeInsets.only(left: 15),
-                    ),
-                    Icon(
-                      Icons.favorite,
-                      color: Colors.red,
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      "1,242",
-                    ),
-                    SizedBox(
-                      width: 15,
-                    ),
-                    Icon(
-                      Icons.mode_comment_outlined,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "24",
-                    )
-                  ],
-                ),
-                Row(children: const [
-                  Icon(Icons.turned_in_not, size: 25),
-                  Padding(
-                    padding: EdgeInsets.only(right: 10),
-                  ),
-                ])
-              ]),
-            ]),
           ),
         ),
       ),
